851e468e3688e634027df09220408eaab2f42e1a
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-13 22:10:17 +0530

    f

--- veh/src/components/Timeline.jsx
+++ veh/src/components/Timeline.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import './styles/Timeline.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import './styles/Timeline.css'; // Custom CSS for styles
 
 function Timeline() {
   useEffect(() => {
@@ -9,29 +9,28 @@ function Timeline() {
   }, []);
 
   const experiences = [
-    { company: 'National Development Bank PLC (NDB Bank )', role: 'Internship at Product Lifecycle Management', date: 'April 2024 - October 2024', logo: 'https://res.cloudinary.com/dwcxwpn7q/image/upload/v1734023279/eehanee/picture_rloqhz.jpg' },
+    { company: 'National Development Bank PLC (NDB Bank)', role: 'Internship at Product Lifecycle Management', date: 'April 2024 - October 2024', logo: 'https://res.cloudinary.com/dwcxwpn7q/image/upload/v1734023279/eehanee/picture_rloqhz.jpg' },
     { company: 'Citizens Development Business Finance PLC', role: 'Customer Service Officer, Deposit Officer, Teller', date: 'December 2019 - August 2021', logo: 'https://res.cloudinary.com/dwcxwpn7q/image/upload/v1734023511/eehanee/images_fgi4f5.png' },
-    
-
   ];
 
   return (
-    <div className="timeline">
-      <h1 className="timeline-title">Experiences</h1>
-      <div className="timeline-container">
+    <div className="timeline-container">
+      <h1 className="timeline-title">EXPERIENCES</h1>
+      <div className="timeline">
+        <div className="vertical-line"></div>
         {experiences.map((exp, index) => (
           <div
             key={index}
-            className={`timeline-item ${index % 2 === 0 ? 'left' : 'right'}`}
             data-aos="fade-up"
+            className="timeline-item"
           >
-            <div className="timeline-dot">
-              <img src={exp.logo} alt={`${exp.company} logo`} />
+            <div className="timeline-logo">
+              <img src={exp.logo} alt={`${exp.company} logo`} className="logo-image" />
             </div>
-            <div className="timeline-content">
-              <h2>{exp.company}</h2>
-              <h4>{exp.role}</h4>
-              <p>{exp.date}</p>
+            <div className="timeline-details">
+              <h2 className="company-name">{exp.company}</h2>
+              <h4 className="role-name">{exp.role}</h4>
+              <p className="date-range">{exp.date}</p>
             </div>
           </div>
         ))}
