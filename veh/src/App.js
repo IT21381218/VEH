e46b196e8fb8ed99b7e301a1727e38b4826f9680
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-14 01:00:01 +0530

    Update App.js

--- veh/src/App.js
+++ veh/src/App.js
@@ -1,3 +1,48 @@
+// import React, { useState, useEffect } from 'react';
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import Loading from './components/Loading';
+
+// function App() {
+//   const [isLoading, setIsLoading] = useState(true);
+
+//   useEffect(() => {
+//     const handleLoad = () => {
+//       // Add a delay of 2 seconds (2000ms) before hiding the loading screen
+//       setTimeout(() => {
+//         setIsLoading(false);
+//       }, 5000); // Adjust this time to make the loading screen appear longer
+//     };
+
+//     if (document.readyState === 'complete') {
+//       handleLoad();
+//     } else {
+//       window.addEventListener('load', handleLoad);
+//     }
+
+//     return () => {
+//       window.removeEventListener('load', handleLoad);
+//     };
+//   }, []);
+
+//   return (
+//     <>
+//       {isLoading ? (
+//         <Loading />
+//       ) : (
+//         <div>
+//           <Header />
+//           <Home />
+//         </div>
+//       )}
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
@@ -11,7 +56,7 @@ function App() {
       // Add a delay of 2 seconds (2000ms) before hiding the loading screen
       setTimeout(() => {
         setIsLoading(false);
-      }, 5000); // Adjust this time to make the loading screen appear longer
+      }, 2000); // Adjust this time to make the loading screen appear longer
     };
 
     if (document.readyState === 'complete') {
