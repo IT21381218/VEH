653e3033266e26a0acc951f6e4d206088302605f
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-14 00:44:44 +0530

    v

--- veh/src/App.js
+++ veh/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
+import Loading from './components/Loading';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const handleLoad = () => {
+      // Add a delay of 2 seconds (2000ms) before hiding the loading screen
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 5000); // Adjust this time to make the loading screen appear longer
+    };
+
+    if (document.readyState === 'complete') {
+      handleLoad();
+    } else {
+      window.addEventListener('load', handleLoad);
+    }
+
+    return () => {
+      window.removeEventListener('load', handleLoad);
+    };
+  }, []);
+
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Header />
+          <Home />
+        </div>
+      )}
+    </>
   );
 }
 
