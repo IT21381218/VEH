75fa3249052fe8ec6abf4bc0f2f2bd8fc006ca05
poornaka <99286090+IT21381218@users.noreply.github.com>
2024-12-17 00:30:48 +0530

    Update Projects.jsx

--- veh/src/components/Projects.jsx
+++ veh/src/components/Projects.jsx
@@ -67,7 +67,7 @@ const CenterImage = () => {
         backgroundSize,
         opacity,
         backgroundImage:
-          "url(https://images.unsplash.com/photo-1460186136353-977e9d6085a1?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+          "url(https://res.cloudinary.com/dwcxwpn7q/image/upload/v1734371660/pexels-eberhardgross-1670187_nnn199.jpg)",
       }}
     />
   );
